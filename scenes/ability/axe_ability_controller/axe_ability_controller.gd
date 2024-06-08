8258a3806bc8538398158dfae190c7ea212acba1
Pablo Hoffmann <pablohoffmann1998@gmail.com>
2024-06-08 00:38:46 -0300

    improving the UI

--- scenes/ability/axe_ability_controller/axe_ability_controller.gd
+++ scenes/ability/axe_ability_controller/axe_ability_controller.gd
@@ -1,11 +1,13 @@
 extends Node
 
 @export var axe_ability_scene: PackedScene
-var damage = 10
+var base_damage = 10
+var addicional_damage_percent = 1
 
 
 func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_timer_timeout():
@@ -20,5 +22,10 @@ func on_timer_timeout():
 	var axe_instance = axe_ability_scene.instantiate() as Node2D
 	foreground.add_child(axe_instance)
 	axe_instance.global_position = player.global_position
-	axe_instance.hitbox_component.damage = damage
+	axe_instance.hitbox_component.damage = base_damage * addicional_damage_percent
 	
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrade: Dictionary):
+	if upgrade.id == "axe_damage":
+		addicional_damage_percent = 1 + (current_upgrade["axe_damage"]["quantity"] * .1)
--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -2,7 +2,8 @@ extends Node
 
 const MAX_RANGE = 150
 @export var sword_ability: PackedScene
-var damage = 5
+var base_damage = 5
+var addicional_damage_percent = 1
 var base_wait_time
 
 # Called when the node enters the scene tree for the first time.
@@ -34,7 +35,7 @@ func on_timer_timeout():
 	var sword_instance = sword_ability.instantiate() as SwordAbility
 	var foreground_layer = get_tree().get_first_node_in_group("foreground_layer")
 	foreground_layer.get_parent().add_child(sword_instance)
-	sword_instance.hitbox_component.damage = damage
+	sword_instance.hitbox_component.damage = base_damage * addicional_damage_percent
 	sword_instance.global_position = enemies[0].global_position
 	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
 	
@@ -43,9 +44,10 @@ func on_timer_timeout():
 
 
 func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrade: Dictionary):
-	if upgrade.id != "sword_rate":
-		return
-	var percent_reduction = current_upgrade["sword_rate"]["quantity"] * .1
-	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
-	$Timer.start()
+	if upgrade.id == "sword_rate":
+		var percent_reduction = current_upgrade["sword_rate"]["quantity"] * .1
+		$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+		$Timer.start()
+	elif upgrade.id == "sword_damage":
+		addicional_damage_percent = 1 + (current_upgrade["sword_damage"]["quantity"] * .15)
 	
